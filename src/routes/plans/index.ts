4aad981864728ab65638cacf388288cb84c249f4
Menashi30 <menashi30@gmail.com>
2024-05-09 17:06:46 -0400

    Adding the recent plan model and index file to fetch data from Mongo DB

---
+++ src/routes/plans/index.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { connectToMongoose } from "~/dBconfig/MongoDb";
+import { Plan } from "~/models/planModel";
+
+// Establish database connection
+connectToMongoose();
+
+export default async (app: FastifyInstance) => {
+  app.get("/user/plans", async (req: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const plans = await Plan.find({}, { _id: false, tripName: true });
+      if (!plans) {
+        return reply.status(404).send({ message: "Plans not found" });
+      }
+      return reply.send(plans);
+    } catch (error) {
+      console.error("Error querying user:", error);
+      return reply.status(500).send({ message: "Internal server error" });
+    }
+  });
+};
