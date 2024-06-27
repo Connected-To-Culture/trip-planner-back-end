0ecd53b0a96d9a320954989ed059bf79fce7faed
Jacob <jacobcons@gmail.com>
2024-06-27 10:05:42 +0000

    prettier, app.ts cleanup - new general error handler

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import helmet from '@fastify/helmet';
 import { fastifyAutoload } from '@fastify/autoload';
 import path from 'path';
 import 'dotenv/config';
-import Cors from "@fastify/cors";
-import { registerGoogleOAuth2Provider, registerFacebookOAuth2Provider } from './routes/auth';
+import Cors from '@fastify/cors';
+import {
+    registerGoogleOAuth2Provider,
+    registerFacebookOAuth2Provider,
+} from './routes/auth';
 
-const port = Number(process.env.PORT) || 4000;
 const app = Fastify({
     logger: {
         transport: {
@@ -16,11 +18,9 @@ const app = Fastify({
     },
     ignoreTrailingSlash: true,
 });
+
+// utility plugins
 app.register(formBody);
-
-registerGoogleOAuth2Provider(app);
-registerFacebookOAuth2Provider(app);
-
 app.register(helmet, {
     global: true,
     contentSecurityPolicy: {
@@ -30,24 +30,39 @@ app.register(helmet, {
         },
     },
 });
-
-const corsOptions = {
+app.register(Cors, {
     origin: '*',
-};
+});
 
-app.register(Cors, corsOptions);
-
+// route plugins
 app.register(fastifyAutoload, {
     dir: path.join(__dirname, 'routes'),
     dirNameRoutePrefix: false,
 });
+registerGoogleOAuth2Provider(app);
+registerFacebookOAuth2Provider(app);
 
 app.setNotFoundHandler((req: FastifyRequest, reply: FastifyReply) => {
     reply.code(404).send({ error: 'Not Found' });
 });
 
+app.setErrorHandler((error, request, reply) => {
+    // log server errors
+    if (error.statusCode === 500) {
+        app.log.error(error);
+    }
+
+    // server error => respond with generic server error in prod => otherwise just respond with the full error
+    const errorResponse =
+        error.statusCode === 500 && process.env.NODE_ENV! === 'production'
+            ? { message: 'internal server error' }
+            : error;
+    return reply.status(error.statusCode).send(errorResponse);
+});
+
 const start = async () => {
     try {
+        const port = Number(process.env.PORT) || 4000;
         await app.listen({ port, host: '0.0.0.0' });
         console.log(`Server listening on port ${port}`);
     } catch (err) {
@@ -55,6 +70,5 @@ const start = async () => {
         process.exit(1);
     }
 };
-
 
 start();
--- src/routes/plans/index.ts
+++ src/routes/plans/index.ts
@@ -1,24 +1,33 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import test from "node:test";
-import { connectToMongoose } from "~/dBconfig/MongoDb";
-import { Plan } from "~/models/planModel";
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import test from 'node:test';
+import { connectToMongoose } from '~/dBconfig/MongoDb';
+import { Plan } from '~/models/planModel';
 
 // Establish database connection
 connectToMongoose();
 
 export default async (app: FastifyInstance) => {
-  app.get("/user/plans", async (req: FastifyRequest, reply: FastifyReply) => {
-  
-    try {
-      const plans = await Plan.find({}, { _id: true, tripName: true,tripStartDate:true,tripEndDate:true,numOfTravelers:true,totalExpense:true });
-     
-      if (!plans) {
-        return reply.status(404).send({ message: "Plans not found" });
-      }
-      return reply.send(plans);
-    } catch (error) {
-      console.error("Error querying user:", error);
-      return reply.status(500).send({ message: "Internal server error" });
-    }
-  });
+    app.get('/user/plans', async (req: FastifyRequest, reply: FastifyReply) => {
+        try {
+            const plans = await Plan.find(
+                {},
+                {
+                    _id: true,
+                    tripName: true,
+                    tripStartDate: true,
+                    tripEndDate: true,
+                    numOfTravelers: true,
+                    totalExpense: true,
+                },
+            );
+
+            if (!plans) {
+                return reply.status(404).send({ message: 'Plans not found' });
+            }
+            return reply.send(plans);
+        } catch (error) {
+            console.error('Error querying user:', error);
+            return reply.status(500).send({ message: 'Internal server error' });
+        }
+    });
 };
